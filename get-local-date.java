e6e4cf8ed48cae90aec4c54eba8faa9529ae6eab
Jeffreymaniac <78237937+Jeffreymaniac@users.noreply.github.com>
2022-02-10 16:44:10 -0500

    Create get-local-date.java

---
+++ get-local-date.java
@@ -0,0 +1,11 @@
+import java.time.LocalDate; // Add the date class
+
+class Main {
+
+  public static void main(String[] args) {
+ 
+    System.out.println("The date is: " + LocalDate.now());
+   
+  }
+ 
+}
